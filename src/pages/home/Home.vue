8b5ab232299243e68b0a1a53b18e49bd78674c00
shi.hao <shi.hao@puscene.com>
2019-02-24 18:20:09 +0800

    ''

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <home-header></home-header>
     <swiper-header></swiper-header>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import SwiperHeader from './components/Swiper'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
-  components: {SwiperHeader, HomeHeader},
+  components: {HomeIcons, SwiperHeader, HomeHeader},
   comments: {
     HomeHeader: HomeHeader,
-    SwiperHeader: SwiperHeader
+    SwiperHeader: SwiperHeader,
+    HomeIcons: HomeIcons
   }
 }
 </script>
