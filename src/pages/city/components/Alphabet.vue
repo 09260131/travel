0cc0422f69fed1c81b44bedb30cb3484cdc5a2bb
shi.hao <shi.hao@puscene.com>
2019-03-02 08:18:16 +0800

    ''

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    /*上下居中*/
+    display: flex
+    flex-direction: column
+    justify-content: center
+    /*上下居中*/
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .44rem
+    /*水平居中*/
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+    /*水平居中*/
+</style>
